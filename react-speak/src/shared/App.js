3347b3ad2a5e56eedd5f9189f9cd7104a4065cdf
sunm-in <sunm309@naver.com>
2021-07-13 16:51:36 +0900

    Update App.js

--- react-speak/src/shared/App.js
+++ react-speak/src/shared/App.js
@@ -3,8 +3,6 @@ import Grid from "../elements/Grid";
 import Header from "../elements/Header";
 import Button from "../elements/Button";
 import Text from "../elements/Text";
-import Button from "../elements/Button";
-import Text from "../elements/Text";
 import Input from "../elements/Input";
 import styled from "styled-components";
 function App() {
@@ -17,29 +15,13 @@ function App() {
           </Text>
         </Header>
         <Grid margin="auto">
-          <Input/>
+          <Input />
         </Grid>
       </Grid>
 
-
-      <Grid bg="#636e72" padding="20px" width="600px" margin="auto">
-
-import Input from "../elements/Input";
-import styled from "styled-components";
-import List from "../components/List";
-function App() {
-  return (
-    <React.Fragment>
-      <Grid width="80%" margin="auto">
-        <Grid margin="auto">
-          <Header bg="#636e72" padding="0px">
-            <Text bold size="60px">
-              항해99 한마디
-            </Text>
-          </Header>
-          <List />
-        </Grid>
-      </Grid>
-
+      <Grid bg="#636e72" padding="20px" width="600px" margin="auto"></Grid>
+    </React.Fragment>
+  );
+}
 
 export default App;
