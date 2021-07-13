cda0ed0fc840c1410c35fe644cb6d6de5a033696
sunm-in <sunm309@naver.com>
2021-07-13 15:13:31 +0900

    Button 적용

--- react-speak/src/shared/App.js
+++ react-speak/src/shared/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Grid from "../elements/Grid";
 import Header from "../elements/Header";
-
+import Button from "../elements/Button";
 import Text from "../elements/Text";
 
 function App() {
@@ -16,6 +16,7 @@ function App() {
       <Grid bg="#636e72" padding="20px" width="600px" margin="auto">
         <Text bold size="24px">test</Text>
       </Grid>
+      <Button text="추가하기" width="300px" margin="12px auto"></Button>
 
     </React.Fragment>
   );
