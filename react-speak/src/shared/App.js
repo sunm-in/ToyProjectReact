5d80cdc91f02088b48729b1cebde2aa61566d206
sunm-in <sunm309@naver.com>
2021-07-13 18:36:36 +0900

    speak

--- react-speak/src/shared/App.js
+++ react-speak/src/shared/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Route} from "react-router-dom";
 import SpeakMain from "../pages/SpeakMain";
+import SpeakDetail from "../pages/SpeakDetail";
 
 function App() {
   return (
     <React.Fragment>
       <Route path="/" exact component={SpeakMain}/>
+      <Route path="/detail" exact component={SpeakDetail}/>
     </React.Fragment>
   );
 }
