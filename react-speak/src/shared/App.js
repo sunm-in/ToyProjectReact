925f0faad1bd6c7abba050937fb2aa8895066a09
sunm-in <sunm309@naver.com>
2021-07-13 14:24:16 +0900

    speak

--- react-speak/src/shared/App.js
+++ react-speak/src/shared/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Grid from "../elements/Grid";
+import Text from "../elements/Text";
 
 function App() {
   return (
     <React.Fragment>
-      <Grid bg="#636e72">test</Grid>
+      <Grid bg="#636e72" padding="20px" width="600px" margin="auto">
+        <Text bold size="24px">test</Text>
+      </Grid>
     </React.Fragment>
   );
 }
