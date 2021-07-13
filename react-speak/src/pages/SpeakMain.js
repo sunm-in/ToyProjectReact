f96b779af6e4033f1929178029a232374fc95471
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-07-13 17:24:27 +0900

    메인페이지 뼈대 완성

--- react-speak/src/pages/SpeakMain.js
+++ react-speak/src/pages/SpeakMain.js
@@ -2,16 +2,22 @@ import React from "react";
 import Grid from "../elements/Grid";
 import Header from "../elements/Header";
 import Text from "../elements/Text";
+import List from "../components/List";
 
 const SpeakMain = () => {
   return (
     <React.Fragment>
-      <Grid bg="#636e72">
-        <Header bg="#636e72" padding="40px">
-          <Text bold size="60px">
-            항해99 한마디
-          </Text>
-        </Header>
+      <Grid width="100%" bg="#b2bec3">
+        <Grid width="80%" margin="auto">
+          <Grid margin="auto">
+            <Header bg="#636e72" padding="3px">
+              <Text bold size="40px">
+                항해99 한마디
+              </Text>
+            </Header>
+            <List />
+          </Grid>
+        </Grid>
       </Grid>
     </React.Fragment>
   );
--- react-speak/src/shared/App.js
+++ react-speak/src/shared/App.js
@@ -1,25 +1,11 @@
 import React from "react";
-import Grid from "../elements/Grid";
-import Header from "../elements/Header";
-import Button from "../elements/Button";
-import Text from "../elements/Text";
-import List from "../components/List";
-import Input from "../elements/Input";
+import {Route} from "react-router-dom";
+import SpeakMain from "../pages/SpeakMain";
 
 function App() {
   return (
     <React.Fragment>
-      <Grid width="80%" margin="auto">
-        <Grid margin="auto">
-          <Header bg="#636e72" padding="0px">
-            <Text bold size="60px">
-              항해99 한마디
-            </Text>
-          </Header>
-          <List />
-        </Grid>
-      </Grid>
-      <Grid bg="#636e72" padding="20px" width="600px" margin="auto"></Grid>
+      <Route path="/" exact component={SpeakMain}/>
     </React.Fragment>
   );
 }
