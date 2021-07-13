013fccbe5a09997ada00d4dbdc421bb98fcef0b8
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-07-13 16:31:08 +0900

    ...충돌..커밋..

--- react-speak/src/elements/Header.js
+++ react-speak/src/elements/Header.js
@@ -2,12 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 const Header = (props) => {
+<<<<<<< Updated upstream
   const { bg, width, padding } = props;
+=======
+  const { bg, width, padding, children, height, margin } = props;
+>>>>>>> Stashed changes
 
   const styles = {
     bg: bg,
     width: width,
     padding: padding,
+    height: height,
+    margin: margin,
   };
   return (
     <React.Fragment>
@@ -19,7 +25,9 @@ const Header = (props) => {
 Header.defaultProps = {
   bg: false,
   width: "100%",
+  height: "30%",
   padding: "4px",
+  margin: false,
 };
 
 const HeaderBox = styled.div`
@@ -27,6 +35,7 @@ const HeaderBox = styled.div`
   ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
   width: ${(props) => props.width};
   box-sizing: border-box;
+  height: 15%;
 `;
 
 export default Header;
--- react-speak/src/shared/App.js
+++ react-speak/src/shared/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Grid from "../elements/Grid";
 import Header from "../elements/Header";
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 import Button from "../elements/Button";
 import Text from "../elements/Text";
+<<<<<<< Updated upstream
 
 function App() {
   return (
@@ -14,10 +19,29 @@ function App() {
     
 
       <Grid bg="#636e72" padding="20px" width="600px" margin="auto">
+=======
+import Input from "../elements/Input";
+import styled from "styled-components";
+import List from "../components/List";
+function App() {
+  return (
+    <React.Fragment>
+      <Grid width="80%" margin="auto">
+        <Grid margin="auto">
+          <Header bg="#636e72" padding="0px">
+            <Text bold size="60px">
+              항해99 한마디
+            </Text>
+          </Header>
+          <List />
+        </Grid>
+      </Grid>
+
+      {/* <Grid bg="#636e72" padding="20px" width="600px" margin="auto">
+>>>>>>> Stashed changes
         <Text bold size="24px">test</Text>
       </Grid>
-      <Button text="추가하기" width="300px" margin="12px auto"></Button>
-
+      <Button text="추가하기" width="300px" margin="12px auto"></Button> */}
     </React.Fragment>
   );
 }
