f5de91c732a4be1dfd9dc44bc346e2bfe6b22edf
sunm-in <sunm309@naver.com>
2021-07-14 16:42:11 +0900

    speak

---
+++ react-speak/src/redux/configureStore.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createBrowserHistory } from "history";
+import speak from "./modules/speak";
+import thunk from "redux-thunk";
+
+export const history = createBrowserHistory();
+
+const middlewares = [thunk.withExtraArgument({ history: history })];
+
+const enhancer = applyMiddleware(...middlewares)
+const rootReducer = combineReducers({ speak });
+const store = createStore(rootReducer, enhancer);
+
+export default store;
